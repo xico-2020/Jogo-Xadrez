15d048ed760b89f450d13c66a49f9b63b8728b4f
Francisco Paulo <fpaulocgd@outlook.pt>
2020-05-11 21:44:50 +0100

    Tratando erros com excecao personalizada

--- Xadrez-console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 
@@ -26,11 +27,45 @@ namespace tabuleiro
             return pecas[linha, coluna]; 
         }
 
+        public Peca peca(Posicao pos) // Sobrecarga do método peca que só recebe pos da classe Posicao.
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos) // Método que passa a posição pos e chama o método validarPosicao. Se não for válida lança exceção.
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p, Posicao pos)   // Método para colocar peca no tabuleiro
         {
+            if (existePeca(pos))  // Faz dois testes, chama método existePeca que por sua vez chama o validarPosicao.
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição"); 
+            }
             pecas[pos.linha, pos.coluna] = p; // Acede a matriz na linha pos.linha, pos.coluna e recebe a peca p .
             p.posicao = pos;  // Informa a posicao da peça p na posição pos.
         } 
+
+        public bool posicaoValida(Posicao pos)  // Valida se o numero de linha e coluna é válido.
+        {
+            if (pos.linha<0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void validarPosicao(Posicao pos)  // método que por cada posicao não válida lança uma exceção personalizada
+        {
+            if (!posicaoValida(pos))   // condição se não for posíção válida (! indica negação)
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
 
     }
 }
