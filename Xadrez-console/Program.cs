6b8954fa425688f25566b5aac262d01c55d95e64
Francisco Paulo <fpaulocgd@outlook.pt>
2020-05-11 22:27:44 +0100

    Criada classe PosicaoXadrez+

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,23 +8,13 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));  // coloca Torre preta na posição  0,0 .
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));  // coloca Torre preta na posição  1,3 .
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));  // coloca Rei preto na posição  2,4 .
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
 
         }
 
-        
+
     }
 }
---
+++ Xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez  // define as posições no tabuleiro
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a'); // converte o numero da linha na posicao da tabela 
+                         // (ex. posicao 3 tabuleiro é 8-3= 5 na tabela (sendo que linha 8 = pos 0 e linha 1 = pos 7).
+                         // (ex. a-a = 0, a-b= 1 na tabela (sendo que coluna a = pos 0 e coluna h = pos 7).
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
